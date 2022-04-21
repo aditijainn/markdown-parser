0ca9ffe0d12bcdb1e1d8d229400aadb6265210ec
acandrad <acandrad@ucsd.edu>
2022-04-21 11:00:20 -0700

    Added new test
    Added a test to the getLinks method

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,5 +1,9 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.*;
 
 
 public class MarkdownParseTest {
@@ -7,5 +11,18 @@ public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testFileMD() throws Exception {
+        String fileContents = Files.readString(Path.of("/Users/alexandrade/Documents/GitHub/markdown-parser/test-file.md"));
+
+        ArrayList<String> testResult = new ArrayList<>();
+        List<String> expectedResult = new ArrayList<>();
+
+        testResult = MarkdownParse.getLinks(fileContents);
+        expectedResult = List.of("https://something.com", "some-thing.html");
+
+        assertEquals(expectedResult, testResult);
     }
 }
